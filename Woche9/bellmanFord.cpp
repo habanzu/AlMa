93690e200f7c59f3733ad0830ad23a5d88791d76
Georg Meinhardt <habanzu@gmail.com>
2019-12-17 23:36:34 +0100

    Add bfs for edges in Woche9

--- Woche9/bellmanFord.cpp
+++ Woche9/bellmanFord.cpp
@@ -1,8 +1,7 @@
 #include "bellmanFord.h"
 
-typedef Graph::NodeId NodeId;
 
-Graph bellmanFord(const Graph graph, const Graph::NodeId start){
+Graph bellmanFord(const Graph graph, const NodeId start){
 
     std::vector<double> l;
     std::vector<NodeId> p;
@@ -16,8 +15,33 @@ Graph bellmanFord(const Graph graph, const Graph::NodeId start){
     p[start] = Graph::invalid_node;
 
     for (unsigned i = 1; i < n; ++i){
-        for (auto e : )
+        for (auto e : bfsEdges(graph, start)){
+            
+        }
     }
 
     return(A);
+}
+
+vector<Neighbor> bfsEdges(Graph graph, NodeId id){
+    vector<NodeId> visited;
+    queue<NodeId> unvisited;
+    vector<Neighbor> neighbors;
+    vector<Neighbor> tmp;
+    unvisited.push(id);
+
+    while(unvisited.empty() == 0){
+        id = unvisited.front();
+        visited.push_back(id);
+        unvisited.pop();
+        tmp = graph.get_node(id).adjacent_nodes();
+        for(auto neighbor : tmp){
+            neighbors.push_back(neighbor);
+            id = neighbor.id();
+            if(!(std::find(visited.begin(), visited.end(), id) != visited.end()))
+                unvisited.push(id);
+        }
+    }
+
+    return neighbors;
 }
--- Woche9/bellmanFord.h
+++ Woche9/bellmanFord.h
@@ -1,3 +1,11 @@
 #include "../Woche7/graph.h"
+#include <queue>
+#include <vector>
+
+typedef Graph::NodeId NodeId;
+typedef Graph::Neighbor Neighbor;
+using std::vector;
+using std::queue;
 
 Graph bellmanFord(Graph graph, Graph::NodeId start);
+vector<Graph::Neighbor> bfsEdges(Graph graph, NodeId id);
